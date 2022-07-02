8aed523099d6c5270aabc1c4dff52c80cd97267f
NChang007 <nelsonc924@gmail.com>
2022-07-02 14:23:05 +0000

    now it works

--- src/models.py
+++ src/models.py
@@ -8,34 +8,15 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-# class Person(Base):
-#     __tablename__ = 'person'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
-
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
-
-#     def to_dict(self):
-#         return {}
-
 class Follower(Base):
+    __tablename__= 'follower'
+    id = Column(Integer, primary_key = True)
     user_from_id = Column(Integer, ForeignKey('user.id'))
     user_to_id = Column(Integer, ForeignKey('user.id'))
 
 class User(Base):
     __tablename__ = 'user'
-    id = Column(Interger, primary_key = True)
+    id = Column(Integer, primary_key = True)
     username = Column(String(256))
     firstname = Column(String(256))
     lastname = Column(String(256))
@@ -43,20 +24,20 @@ class User(Base):
 
 class Post(Base):
     __tablename__ = 'post'
-    id = Column(Interger, primary_key = True)
+    id = Column(Integer, primary_key = True)
     user_id = Column(Integer, ForeignKey('user.id'))
 
 class Media(Base):
     __tablename__ = "media"
-    id = Column(Interger, primary_key = True)
+    id = Column(Integer, primary_key = True)
     mediatype = Column(String(256))
     url = Column(String(256))
     post_id = Column(Integer, ForeignKey('post.id'))
 
 class Comment(Base):
     __tablename__ = 'comments'
-    id = Column(Interger, primary_key = True)
-    author_id = Column(Interger, ForeignKey('user.id'))
+    id = Column(Integer, primary_key = True)
+    author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
 
 ## Draw from SQLAlchemy base
